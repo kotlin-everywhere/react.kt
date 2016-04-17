3a085f804ebcb687d9d933fe7d2815a58a719dcc
Sung Min Park <vamf12@gmail.com>
2016-04-18 06:43:21 +0900

    add stateless component

--- src/main/kotlin/com/github/kotlin_everywhere/react/react.kt
+++ src/main/kotlin/com/github/kotlin_everywhere/react/react.kt
@@ -7,9 +7,16 @@ interface ReactElement
 @native
 object React {
     fun createElement(elementName: String, prop: Any?, vararg children: Any?): ReactElement
+    fun createElement(statelessComponent: () -> ReactElement?, prop: Any?, vararg children: Any?): ReactElement
 }
 
 @native
 object ReactDOM {
     fun render(reactElement: ReactElement, element: Element)
+}
+
+fun stateless(component: () -> ReactElement?): () -> ReactElement {
+    return {
+        React.createElement(component, null)
+    }
 }
--- src/test/kotlin/com/github/kotlin_everywhere/react/test/IntegrationTest.kt
+++ src/test/kotlin/com/github/kotlin_everywhere/react/test/IntegrationTest.kt
@@ -2,6 +2,7 @@ package com.github.kotlin_everywhere.react.test
 
 import com.github.kotlin_everywhere.react.Div
 import com.github.kotlin_everywhere.react.ReactDOM
+import com.github.kotlin_everywhere.react.stateless
 import org.junit.Test
 import org.w3c.dom.Element
 import kotlin.browser.document
@@ -15,6 +16,17 @@ class IntegrationTest {
     @Test
     fun testRender() {
         ReactDOM.render(Div { +"Hello, World!" }, fixture)
+        assertEquals(1, fixture.childElementCount)
+        assertEquals("Hello, World!", fixture.children().first().textContent)
+    }
+
+    @Test
+    fun testStateless() {
+        val HelloWorld = stateless {
+            Div { +"Hello, World!" }
+        }
+
+        ReactDOM.render(HelloWorld(), fixture)
         assertEquals(1, fixture.childElementCount)
         assertEquals("Hello, World!", fixture.children().first().textContent)
     }
